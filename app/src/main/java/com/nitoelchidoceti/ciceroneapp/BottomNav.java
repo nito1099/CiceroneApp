e5a051827e03b6701aad2db3d339b314a8b54a14
Juan Pablo Rodríguez Gutiérrez <juanpa1099@hotmail.com>
2019-10-14 19:15:09 -0500

    Toolbar con icono de QR y Cicerone

--- app/src/main/java/com/nitoelchidoceti/ciceroneapp/BottomNav.java
+++ app/src/main/java/com/nitoelchidoceti/ciceroneapp/BottomNav.java
@@ -2,13 +2,17 @@ package com.nitoelchidoceti.ciceroneapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.nitoelchidoceti.ciceroneapp.Fragments.AccountFragment;
 import com.nitoelchidoceti.ciceroneapp.Fragments.GuidesFragment;
@@ -28,7 +32,28 @@ public class BottomNav extends AppCompatActivity {
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
                 new HomeFragment()).commit();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_actionbar,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == R.id.qr_code){
+            launchQr();
+        }
+        return true;
+    }
+
+    private void launchQr() {//CODIGO QR ACTIVITY
+        Toast.makeText(getApplicationContext(),"Se presionó QR",Toast.LENGTH_SHORT).show();
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
@@ -66,5 +91,12 @@ public class BottomNav extends AppCompatActivity {
         Intent launchLoginFromAccount = new Intent(this,LoginActivity.class);
         launchLoginFromAccount.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(launchLoginFromAccount);
+    }
+
+    public void launchHomeFragment(View view) {
+        BottomNavigationView bottomNav = findViewById(R.id.menu_bottom_navigation);
+        bottomNav.getMenu().findItem(R.id.nav_home).setChecked(true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
 }
