3674d27bd9c0f86c6bac98d2c331f2f2985eef1c
Juan Pablo Rodríguez Gutiérrez <juanpa1099@hotmail.com>
2019-11-10 20:52:05 -0600

    Map Fragment funcionado! c;

--- app/src/main/java/com/nitoelchidoceti/ciceroneapp/Fragments/MapFragment.java
+++ app/src/main/java/com/nitoelchidoceti/ciceroneapp/Fragments/MapFragment.java
@@ -9,14 +9,50 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.nitoelchidoceti.ciceroneapp.R;
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mGoogleMap;
+    MapView mapView;
+    View mView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map,container,false);
+        mView = inflater.inflate(R.layout.fragment_map,container,false);
+
+        return mView;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapView = mView.findViewById(R.id.mapa);
+        if (mapView != null){
+            mapView.onCreate(null);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mGoogleMap = googleMap;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(20.677056, -103.347000)).title("Catedral de Guadalajara").snippet("La Madre de Guadalajara"));
+        CameraPosition catedral = CameraPosition.builder().target(new LatLng(20.677056, -103.347000)).zoom(16).bearing(0).tilt(45).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(catedral));
     }
 }
